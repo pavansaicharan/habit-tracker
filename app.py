7d852270fe47ed296c3ebb31525fc958d57a5f23
pavansaicharan <nayudu.0104@gmail.com>
2024-09-25 12:34:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 import os
 from flask import Flask
 from routes import pages
-from pymongo import  MongoClient
-from dotenv  import load_dotenv
-
+from pymongo import MongoClient
+from dotenv import load_dotenv
 
 load_dotenv()
-
-
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(pages)
     client = MongoClient(os.environ.get("MONGODB_URI"))
     
-    app.db=client.get_default_database()
+    app.db = client.get_default_database()
     
     return app
+
+# Expose the app instance directly
+app = create_app()
